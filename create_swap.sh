f8026ee4ee5607054e8d495a2eeaf8b22f4e0289
BaoChinh07 <baochinh0211@gmail.com>
2025-04-17 16:37:34 +0700

    Edit create_swap.sh

--- create_swap.sh
+++ create_swap.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# Lưu URL của chính script để gợi ý lại
+SCRIPT_URL="https://raw.githubusercontent.com/BaoChinh07/swap-script/master/create_swap.sh"
+
 SIZE_MB=$1
 
 if [ -z "$SIZE_MB" ]; then
   echo "❌ Vui lòng truyền dung lượng swap (MB). Ví dụ: 2048"
+  echo "👉 Ví dụ: bash <(curl -s $SCRIPT_URL) 2048"
+  exit 1
+else
+
+if ! [[ "$SIZE_MB" =~ ^[0-9]+$ ]]; then
+  echo "❌ Dung lượng swap phải là một số nguyên (MB). Ví dụ: 2048"
+  echo "👉 Ví dụ: bash <(curl -s $SCRIPT_URL) 2048"
   exit 1
 fi
+
 
 echo "🛠️ Đang tạo swap file với dung lượng ${SIZE_MB}MB..."
 
